75283a45b3da974a9caeb40701aedb5085d5682f
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-22 18:40:24 +0300

    "Updated ProductDetailPage to accept props, added loading state, and implemented getStaticPaths; updated HomePage to link to product IDs instead of product objects"

--- pages/[pid].js
+++ pages/[pid].js
@@ -3,8 +3,12 @@ import fs from "fs/promises";
 import { notFound, redirect } from "next/navigation";
 import path from "path";
 
-function ProductDetailPage() {
+function ProductDetailPage(props) {
   const { loadedProduct } = props;
+
+  if (!loadedProduct) {
+    return <p>Loading....</p>;
+  }
 
   return (
     <Fragment>
@@ -14,19 +18,43 @@ function ProductDetailPage() {
   );
 }
 
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData.toString());
+
+  return data;
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
   const productId = params.pid;
 
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData.toString());
+  const data = await getData();
+
   const product = data.products.find((product) => product.id === productId);
+
+  if (!product) {
+    return { notFound: true };
+  }
 
   return {
     props: {
       loadedProduct: product,
     },
+  };
+}
+
+export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
+  return {
+    paths: pathsWithParams,
+    fallback: true,
   };
 }
 

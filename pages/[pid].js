b19e3abdd07014748c50a45a3f337869c95c50fd
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-22 17:59:13 +0300

    "Updated getStaticProps function to accept context, added redirect and notFound handling, and modified return values"

---
+++ pages/[pid].js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+function ProductDetailPage() {
+  return (
+    <Fragment>
+      <h1>title</h1>
+      <p>description</p>
+    </Fragment>
+  );
+}
+
+export async function getStaticProps(context) {}
+
+export default ProductDetailPage;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import fs from "fs/promises";
+import { notFound, redirect } from "next/navigation";
 import path from "path";
 
 function HomePage(props) {
@@ -13,15 +14,30 @@ function HomePage(props) {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
+  console.log("(Re-)Generating...");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData.toString());
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
 
   return {
     props: {
       products: data.products,
     },
+    revalidate: 10,
+    redirect: "/",
   };
 }
 
